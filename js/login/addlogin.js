75381b3064973aa175e8d8e39d79ef67b96ea2a2
xzwei <xuzhiwei>
2022-02-22 16:55:32 +0800

    111

---
+++ js/login/addlogin.js
@@ -0,0 +1,12 @@
+function login(){
+    var email=document.getElementById("email");
+    var password=document.getElementById("password");
+
+
+    if(email.Value=="" || password.Value==""){
+        alert("账号密码不能为空")
+
+    }
+    
+
+}
